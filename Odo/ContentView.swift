d40713d3ff1b4f73b8babe18227a525e2cd5f587
samueluy <suy@nmblr.ai>
2026-04-09 11:04:34 +0800

    Initial Commit

---
+++ Odo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Odo
+//
+//  Created by Samuel Jedidiah Uy on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Odo/OdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdoApp.swift
+//  Odo
+//
+//  Created by Samuel Jedidiah Uy on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct OdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
